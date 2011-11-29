85b0777a56ca0bbb5bfb96905de73abfb57cf6c7
Ian Renyard <ian@renyard.net>
2011-11-29 22:30:37 +0000

    Added support for loading as an AMD module.

--- src/footer/core.js
+++ src/footer/core.js
@@ -1,1 +1,19 @@
 Glow.complete('core', '@SRC@');
+
+//	Define the AMD module.
+if (typeof define === 'function' && define.amd) {
+	//	Keep glow in a closure after deleting globals.
+	(function(g) {
+		function amdModule(g) {
+			return g;
+		};
+
+		//	Define major and specific versions.
+		define('glow-2', amdModule);
+		define('glow-2.0.0', amdModule);
+	})(glow);
+
+	//	Delete globals.
+	delete window.Glow;
+	delete window.glow;
+};
